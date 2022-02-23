b2e7c892f674bd893bc9238b1776b19271741ce0
Takasur Azeem <takasurazeem@gmail.com>
2022-02-23 22:42:41 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Takasur Azeem on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
