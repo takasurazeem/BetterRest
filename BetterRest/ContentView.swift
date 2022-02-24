5e5aaf798e86f71be20b33e78cf52c989e91ddb2
Takasur Azeem <takasurazeem@gmail.com>
2022-02-24 17:21:29 +0500

    redesign layout and add calculate button
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,17 +11,34 @@ struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        Form {
-            Section {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            }
-            Section {
-                DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
+                
+                Text("Desired Amount of Sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .padding()
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
             }
         }
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
