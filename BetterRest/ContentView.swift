e50471eeee8f4940c45c5c734800eabff1026e8a
Takasur Azeem <takasurazeem@gmail.com>
2022-02-24 23:24:26 +0500

    Connects SwiftUI to Core ML
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Takasur Azeem on 23/02/2022.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,10 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -34,11 +39,33 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("Ok") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = Double((components.hour ?? 0) * 60 * 60)
+            let minute = Double((components.minute ?? 0) * 60)
+            
+            let prediction = try model.prediction(wake: hour + minute, estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
