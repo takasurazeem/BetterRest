aecef31a333816794575727406ad1dd64af1b886
Takasur Azeem <takasurazeem@gmail.com>
2022-02-25 08:55:08 +0500

    1. Replace each VStack in our form with a Section! 2. Replace the “Number of cups” stepper with a Picker showing the same range of values. 3. Change the user interface so that it always shows their recommended bedtime using a nice and large font. You should be able to remove the “Calculate” button entirely.
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,40 +11,68 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
-    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired Amount of Sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            Form {
+                Section {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .onChange(of: wakeUp) { _ in
+                            calculateBedtime()
+                        }
+                }
+                Section {
+                    Text("Desired Amount of Sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25) { change in
+                        if change {
+                            calculateBedtime()
+                        }
+                    }
+                        
+                }
+                Section {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+//                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text($0 == 1 ? "1 cup" : "\($0) cups")
+                        }
+                    }
+                }
+                Section {
+                    VStack {
+                        Text(alertTitle)
+                            .font(.title).bold()
+                        Text(alertMessage)
+                            .font(.largeTitle).bold()
+                    }
+                }
+                .frame(maxWidth: .infinity)
             }
-            .padding()
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
+            .onSubmit {
+                calculateBedtime()
             }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("Ok") {}
-            } message: {
-                Text(alertMessage)
-            }
+            .onAppear(perform: calculateBedtime)
         }
+        .navigationViewStyle(.stack)
     }
     
     func calculateBedtime() {
@@ -65,7 +93,6 @@ struct ContentView: View {
             alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
     }
 }
 
